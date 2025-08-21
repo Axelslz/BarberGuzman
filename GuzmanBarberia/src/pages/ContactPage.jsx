63b548668301314ebee60d41f1bea3644ec113e8
Axelslz <axlsalazarr1602@gmail.com>
2025-08-21 13:54:35 -0600

    update63

--- GuzmanBarberia/src/pages/ContactPage.jsx
+++ GuzmanBarberia/src/pages/ContactPage.jsx
@@ -18,6 +18,12 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 import Header from '../components/Header.jsx';
 
+const TikTokSvgIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="text-white text-xl w-6 h-6">
+        <path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17A210.06,210.06,0,0,1,448,209.91Z" fill="currentColor"/>
+    </svg>
+);
+
 let DefaultIcon = L.icon({
     iconUrl: icon,
     shadowUrl: iconShadow,
